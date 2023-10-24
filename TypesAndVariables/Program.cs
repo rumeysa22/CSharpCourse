93284b80d920e181aeee676662a2497bf16c2a19
rumeysa <rumeysaakkaya22@gmail.com>
2023-10-24 21:32:06 +0300

    first commit

---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesAndVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 2147483647;
+            Console.WriteLine("number1 is", number1);
+            Console.ReadLine();
+        }
+    }
+}
