45064001cc5bf66b61092b59cd6ff42f93bb2b7d
rumeysa <rumeysaakkaya22@gmail.com>
2023-10-25 14:54:05 +0300

    int

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -11,26 +11,7 @@ namespace TypesAndVariables
         static void Main(string[] args)
         {
             int number1 = 2147483647; //32bit
-            long number2 = 9223372036854775807;  //64bit
-            short number3 = 32767;
-            byte number4 = 255;
-            bool condition1 = false;
-            char character = 'A';
-            double number5 = 10.4; //64bit
-            string city = "Ankara";
-            decimal number6 = 10.4m;
-            Console.WriteLine("number1 is {0}", number1);
-            Console.WriteLine("number2 is {0}", number2);
-            Console.WriteLine("number3 is {0}", number3);
-            Console.WriteLine("number4 is {0}", number4);
-            Console.WriteLine("Character is : {0}", (int)character);
-            Console.WriteLine((int)Days.Wednesday);
-            Console.ReadLine();
 
         }
-    }
-    enum Days
-    {
-        Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
     }
 }
