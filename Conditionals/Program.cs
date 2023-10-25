b3d5b1f9b226abc6f1ded45f5506c84c9a706b52
rumeysa <rumeysaakkaya22@gmail.com>
2023-10-25 16:19:17 +0300

    single line if

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,16 +10,8 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            var number = 11;
-            if (number == 10)
-            {
-                Console.WriteLine("number is 10");
-            }
-            else
-            {
-                Console.WriteLine("number is not 10");
-            }
-            Console.ReadLine();
+            var Number = 10;
+            Console.WriteLine(Number == 10 ? "Number is 10" : "Number is not 10");
         }
     }
 }
