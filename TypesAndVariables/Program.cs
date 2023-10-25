a243f80191c6e0fc4225399f743fd369a0dd8b99
rumeysa <rumeysaakkaya22@gmail.com>
2023-10-25 12:14:04 +0300

    veri tipleri

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -10,8 +10,18 @@ namespace TypesAndVariables
     {
         static void Main(string[] args)
         {
-            int number1 = 2147483647;
-            Console.WriteLine("number1 is", number1);
+            int number1 = 2147483647; //32bit
+            long number2 = 9223372036854775807;  //64bit
+            short number3 = 32767;
+            byte number4 = 255;
+            bool condition1 = false;
+            char character = 'A';
+            string city = "Ankara";
+            Console.WriteLine("number1 is {0}", number1);
+            Console.WriteLine("number2 is {0}", number2);
+            Console.WriteLine("number3 is {0}", number3);
+            Console.WriteLine("number4 is {0}", number4);
+            Console.WriteLine("Character is : {0}", (int)character);
             Console.ReadLine();
         }
     }
