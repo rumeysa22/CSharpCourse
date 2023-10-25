7490b376cfc63dc1c49e6c2028255429504a71a3
rumeysa <rumeysaakkaya22@gmail.com>
2023-10-25 15:10:09 +0300

    int

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -11,9 +11,7 @@ namespace TypesAndVariables
         static void Main(string[] args)
         {
             int number1 = 2147483647; //32bit
-            long number2 = 9223372036854775807;  //64bit
             Console.WriteLine("number1 is {0}", number1);
-            Console.WriteLine("number2 is {0}", number2);
         }
     }
 }
