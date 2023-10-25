8df4d87989ab3d483c6a1bd716bc40b10792776e
rumeysa <rumeysaakkaya22@gmail.com>
2023-10-25 15:19:12 +0300

    byte

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -11,7 +11,14 @@ namespace TypesAndVariables
         static void Main(string[] args)
         {
             int number1 = 2147483647; //32bit
+            long number2 = -9223372036854775808;
+            short number3 = 32767;
+            byte number4 = 255;
+            bool condition1 = false;
             Console.WriteLine("number1 is {0}", number1);
+            Console.WriteLine("number2 is {0}", number2);
+            Console.WriteLine("number3 is {0}", number3);
+            Console.WriteLine("number4 is {0}", number4);
         }
     }
 }
