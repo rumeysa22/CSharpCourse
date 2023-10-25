2ec31525d4d6d3ce73705745957c88be31de6a2c
rumeysa <rumeysaakkaya22@gmail.com>
2023-10-25 16:36:29 +0300

    switch

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -11,18 +11,31 @@ namespace Conditionals
         static void Main(string[] args)
         {
             var number = 11;
-            if (number == 10)
-            {
-                Console.WriteLine("number is 10");
-            }
+            //if (number == 10)
+            //{
+            //    Console.WriteLine("number is 10");
+            //}
 
-            else if (number == 20)
+            //else if (number == 20)
+            //{
+            //    Console.WriteLine("number is 20");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("number is not 10 or 20");
+            //}
+
+            switch (number)
             {
-                Console.WriteLine("number is 20");
-            }
-            else
-            {
-                Console.WriteLine("number is not 10 or 20");
+                case 10:
+                    Console.WriteLine("number is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("number is 20");
+                    break;
+                default:
+                    Console.WriteLine("Number is not 10 or 20");
+                    break;
             }
             Console.ReadLine();
         }
