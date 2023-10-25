c2309864a84cd33aa89f0b0905845230c29346e6
rumeysa <rumeysaakkaya22@gmail.com>
2023-10-26 00:26:42 +0300

    default parametreli methodlar

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -15,7 +15,7 @@ namespace Methods
             Add();
             Add();
             Add();
-            var result = Add2(20,30);
+            var result = Add2();
             Console.WriteLine(result);
             Console.ReadLine();
         }
@@ -25,7 +25,7 @@ namespace Methods
             Console.WriteLine("Added!!!");
         }
 
-        static int Add2(int number1, int number2)
+        static int Add2(int number1 = 20, int number2 = 30)
         {
             var result = number1 + number2;
             return result;
