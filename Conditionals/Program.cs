1bafcaa855f21962a50ce21591b5edd8b8cc9ef9
rumeysa <rumeysaakkaya22@gmail.com>
2023-10-25 23:53:09 +0300

    metotlar

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -39,19 +39,22 @@ namespace Conditionals
             //        break;
             //}
 
-            if (number >= 0 && number <= 100)
-            {
-                Console.WriteLine("number is between 0-100");
-            }
-            else if (number > 100 && number <= 200)
-            {
-                Console.WriteLine("number is between 101-200");
-            }
-            else if (number > 200 || number < 0)
-            {
-                Console.WriteLine("number is less than 0 or greater than 200");
-            }
-            Console.ReadLine();
+        //    if (number >= 0 && number <= 100)
+        //    {
+        //        Console.WriteLine("number is between 0-100");
+        //    }
+        //    else if (number > 100 && number <= 200)
+        //    {
+        //        Console.WriteLine("number is between 101-200");
+        //    }
+        //    else if (number > 200 || number < 0)
+        //    {
+        //        Console.WriteLine("number is less than 0 or greater than 200");
+        //    }
+
+
+              Console.ReadLine();
         }
+
     }
 }
---
+++ Methods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();
+            Add();
+            Add();
+            Add();
+            Console.ReadLine();
+        }
+
+        static void Add()s
+        {
+            Console.WriteLine("Added!!!");
+        }
+    }
+}
