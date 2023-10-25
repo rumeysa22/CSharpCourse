710ae29294c047754bfaacf3de564005ba441329
rumeysa <rumeysaakkaya22@gmail.com>
2023-10-26 00:01:39 +0300

    parametreli metotlar

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -15,12 +15,20 @@ namespace Methods
             Add();
             Add();
             Add();
+            var result = Add2(20,30);
+            Console.WriteLine(result);
             Console.ReadLine();
         }
 
-        static void Add()s
+        static void Add()
         {
             Console.WriteLine("Added!!!");
+        }
+
+        static int Add2(int number1, int number2)
+        {
+            var result = number1 + number2;
+            return result;
         }
     }
 }
