52d4720f302e480cbb921e42a16134498bf70a8a
rumeysa <rumeysaakkaya22@gmail.com>
2023-10-25 15:27:38 +0300

    decimal

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -17,6 +17,8 @@ namespace TypesAndVariables
             bool condition1 = false;
             char character = 'A';
             double number5 = 10.4; //64bit
+            string city = "Ankara";
+            decimal number6 = 10.4m;
             Console.WriteLine("number1 is {0}", number1);
             Console.WriteLine("number2 is {0}", number2);
             Console.WriteLine("number3 is {0}", number3);
