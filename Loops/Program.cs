2e0973539979b927409727b75f2a7653b0aeef8a
rumeysa <rumeysaakkaya22@gmail.com>
2023-10-26 10:24:55 +0300

    asal sayı

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -19,22 +19,53 @@ namespace Loops
 
             //while (number >= 11);
 
-            string[] students = new string[3] { "rum", "ey", "sa" };
-            foreach (string student in students)
+            //string[] students = new string[3] { "rum", "ey", "sa" };
+            //foreach (string student in students)
+            //{
+            //    Console.WriteLine(student);
+            //}
+            //Console.ReadLine();
+
+
+
+        }
+
+        if (IsPrimeNumber(6))
+        { 
+            Console.WriteLine("this is a prime  number");
+        }
+        else
+        {
+            Console.WriteLine("this is not a prime number");
+        }
+        Console.ReadLine();
+
+        private static bool IsPrimeNumber(int number)
+        {
+            bool result = true;
+            for (int i = 2; i < number - 1; i++)
             {
-                Console.WriteLine(student);
+                if (number % i == 0)
+                {
+                    result = false;
+                    i = number;
+                }
             }
-            Console.ReadLine();
             
         }
 
-        private static void  forLoop()
-        {
-            for (int i = 100; i >= 0; i = i - 2)
-            {
-                Console.WriteLine(i);
-            }
-            Console.WriteLine("finished");
-        }
+
+
+
+
+
+        //private static void  forLoop()
+        //{
+        //    for (int i = 100; i >= 0; i = i - 2)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //    Console.WriteLine("finished");
+        //}
     }
 }
