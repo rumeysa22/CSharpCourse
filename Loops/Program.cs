f8b64986964714d747363cb5edb1dab84dd61c4d
rumeysa <rumeysaakkaya22@gmail.com>
2023-10-26 09:35:24 +0300

    loops

---
+++ Loops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 100;  i >= 0; i = i-2) 
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("finished");
+            Console.ReadLine();
+        }
+    }
+}
