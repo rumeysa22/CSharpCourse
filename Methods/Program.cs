ce836f89fc78721a9344a4200dc90f3b3e217f8c
rumeysa <rumeysaakkaya22@gmail.com>
2023-10-26 07:55:18 +0300

    params

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -26,6 +26,7 @@ namespace Methods
 
             Console.WriteLine(Multiply(2,4));
             Console.WriteLine(Multiply(2,4,5));
+            Console.WriteLine(Add4(2,3,4,5,6));
 
             Console.ReadLine();
         }
@@ -54,5 +55,10 @@ namespace Methods
         {
             return number1 * number2 * number3;
         }
+        static int Add4(params int[] numbers)
+        {
+            return numbers.Sum();
+        }
+
     }
 }
