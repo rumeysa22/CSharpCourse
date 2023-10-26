7df93c9a3776e397a42bb4c3319cb515a70892eb
rumeysa <rumeysaakkaya22@gmail.com>
2023-10-26 08:39:46 +0300

    diziler

---
+++ Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[] { "x", "y", "z" };
+
+            string[] students2 = { "x", "y", "z" };
+            students2[3] = "d";
+
+            foreach (var student in students2)
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
