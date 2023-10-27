20f7b5e5ed82f7608605265b3bfb5a90e63afd06
rumeysa <rumeysaakkaya22@gmail.com>
2023-10-27 23:56:29 +0300

    inheritance

---
+++ inheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Customer { FirstName = "rumeysa" }, new Student { FirstName = "amine" }, new Person { FirstName = "meva" }
+        };
+        foreach(var person in persons)
+        {
+            Console.WriteLine(person.FirstName);
+        };
+        
+        Console.ReadLine();
+        
+        class Person
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+
+        class Customer : Person
+        {
+            public string City { get; set; }
+        }
+        class Student : Person
+        {
+            public string Department { get; set; }
+        }
+    }
+}
