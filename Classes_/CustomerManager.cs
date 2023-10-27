1fc669ff3c59de25fdfd6c7e09c53e44169ad273
rumeysa <rumeysaakkaya22@gmail.com>
2023-10-27 09:21:56 +0300

    classes

---
+++ Classes_/CustomerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_
+{
+    internal class CustomerManager
+    {
+        class CustomerManager
+
+        {
+            public void Add()
+            {
+                Console.WriteLine("Customer added!");
+            }
+
+            public void Update()
+            {
+                Console.WriteLine("Customer Update!");
+            }
+        }
+}
---
+++ Classes_/ProductManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_
+{
+    internal class ProductManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("product added!");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("product Update!");
+        }
+    }
+}
