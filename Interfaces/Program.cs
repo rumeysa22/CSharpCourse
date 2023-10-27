bb729184f7bbe49ad8b1af42e423cb2f839aadcc
rumeysa <rumeysaakkaya22@gmail.com>
2023-10-27 14:06:06 +0300

    interface

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -10,29 +10,47 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
+            PersonManager manager = new PersonManager();
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "rumeysa",
+                LastName = "akkaya",
+                Address = "istanbul"
+            };
+            manager.Add(customer);
+            manager.Add(student);
+            Console.ReadLine();
         }
     }
 
     interface IPerson //interfacelerde temel amaç ana bir nesne olurşturmaktır ,
     {
         int Id { get; set; }
-        int FirstName { get; set; }
-        int LastName { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
     }
 
     class Customer : IPerson
     {
         public int Id { get; set; }
-        public int FirstName { get; set; }
-        public int LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Address { get; set; }
     }
     class student : IPerson
     {
         public int Id { get; set; }
-        public int FirstName { get; set; }
-        public int LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Depatment { get; set; }
+    }
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
     }
 
 }
